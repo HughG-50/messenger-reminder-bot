b340cc0802c394c54cb90f901c8028e771376331
HughG-50 <hugh.greethead@gmail.com>
2022-02-05 20:07:17 +1100

    Test deploy

--- README.md
+++ README.md
@@ -38,3 +38,59 @@ response = requests.get('https://httpbin.org/ip')
 
 print('Your IP is {0}'.format(response.json()['origin']))
 ```
+
+## Deploying to Heroku
+
+1. Create new app in Heroku
+2. Update settings > Add Buildpack > Python
+3. Setup `Procfile`
+
+```
+web: python <your_script_name.py>
+worker: python <your_script_name.py>
+```
+
+4. Setup `requirements.txt` file
+
+https://drgabrielharris.medium.com/python-how-create-requirements-txt-using-pipenv-2c22bbb533af
+
+```
+pipenv lock -r > requirements.txt
+```
+
+Or just from global packages
+
+```
+pip3 freeze > requirements.txt
+```
+
+5. Setup heroku with code
+   1. `heroku login`
+   2. `heroku git:remote -a <name_of_heroku_app>`
+   3. Make a commit then push, `git push heroku master`
+
+### Installing Chrome and Chrome driver on Heroku
+
+https://www.andressevilla.com/running-chromedriver-with-python-selenium-on-heroku/
+
+1. Add the following to the config for using Chromedrive in your python script
+
+```
+options = webdriver.ChromeOptions()
+options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+options.add_argument("--headless")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--no-sandbox")
+browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=options)
+```
+
+2. Add the buildpacks to Heroku needed for Chromedriver
+
+- Go to settings > Add Buildpack > Then paste in the URL of the Buildpack you need
+- https://github.com/heroku/heroku-buildpack-google-chrome
+- https://github.com/heroku/heroku-buildpack-chromedriver
+
+3. Add config needed Chromedriver config vars to Heroku
+
+`CHROMEDRIVER_PATH` = `/app/.chromedriver/bin/chromedriver`
+`GOOGLE_CHROME_BIN` = `/app/.apt/usr/bin/google-chrome`
--- main.py
+++ main.py
@@ -7,46 +7,54 @@ from selenium.webdriver.common.keys import Keys
 
 load_dotenv()
 options = webdriver.ChromeOptions()
-options.add_argument("start-maximized")
-options.add_argument("disable-infobars")
-options.add_argument("--disable-extensions")
-browser = webdriver.Chrome(options=options)
+options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+options.add_argument("--headless")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--no-sandbox")
+browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=options)
 
-username = os.getenv('USERNAME')
-password = os.getenv('PASSWORD')
+browser.get("https://www.google.com")
+print(browser.page_source)
 
-browser.get("https://www.messenger.com/login")
-print(f"Navigated to {browser.title}")
+# ###############
+# REMOVE COMMENTS ONCE WORKING ON HEROKU
+# ###############
 
-time.sleep(5)
-email_input = browser.find_element(By.ID, "email")
-password_input = browser.find_element(By.ID, "pass")
-login_button = browser.find_element(By.ID, "loginbutton")
+# username = os.getenv('USERNAME')
+# password = os.getenv('PASSWORD')
 
-email_input.send_keys(username)
-time.sleep(3)
-print(email_input.get_attribute("id"))
-print(email_input.get_attribute("value"))
+# browser.get("https://www.messenger.com/login")
+# print(f"Navigated to {browser.title}")
 
-password_input.send_keys(password)
-time.sleep(4)
-print(password_input.get_attribute("id"))
-print(bool(password_input.get_attribute("value")))
+# time.sleep(5)
+# email_input = browser.find_element(By.ID, "email")
+# password_input = browser.find_element(By.ID, "pass")
+# login_button = browser.find_element(By.ID, "loginbutton")
 
-login_button.click()
-time.sleep(11)
+# email_input.send_keys(username)
+# time.sleep(3)
+# print(email_input.get_attribute("id"))
+# print(email_input.get_attribute("value"))
 
-chat_list_container = browser.find_element(By.CSS_SELECTOR, "[aria-label=Chats]")
-chat_first_element = chat_list_container.find_element(By.CSS_SELECTOR, "div[data-testid='mwthreadlist-item-open']")
-chat_first_element.click()
-time.sleep(3)
+# password_input.send_keys(password)
+# time.sleep(4)
+# print(password_input.get_attribute("id"))
+# print(bool(password_input.get_attribute("value")))
 
-chat_input = browser.find_element(By.XPATH, "//div[@role = 'textbox']")
-chat_input.send_keys("📱📱 Post your screentime screenshots 🧐 🧐 ")
-time.sleep(5)
-submit_button = browser.find_element(By.XPATH, "//div[@aria-label='Press Enter to send']")
+# login_button.click()
+# time.sleep(11)
 
-submit_button.click()
-time.sleep(10)
+# chat_list_container = browser.find_element(By.CSS_SELECTOR, "[aria-label=Chats]")
+# chat_first_element = chat_list_container.find_element(By.CSS_SELECTOR, "div[data-testid='mwthreadlist-item-open']")
+# chat_first_element.click()
+# time.sleep(3)
 
-browser.quit()
+# chat_input = browser.find_element(By.XPATH, "//div[@role = 'textbox']")
+# chat_input.send_keys("📱📱 Post your screentime screenshots 🧐 🧐 ")
+# time.sleep(5)
+# submit_button = browser.find_element(By.XPATH, "//div[@aria-label='Press Enter to send']")
+
+# submit_button.click()
+# time.sleep(10)
+
+# browser.quit()
